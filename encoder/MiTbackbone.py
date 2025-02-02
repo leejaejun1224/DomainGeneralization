eea05f6c8e65679843c3a3b5366c8d340f0df18f
leejaejun1224 <leejaejun122@naver.com>
2025-02-02 17:36:03 +0900

    building block module

--- encoder/MiTbackbone.py
+++ encoder/MiTbackbone.py
@@ -4,31 +4,6 @@ import torch.nn as nn
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 
 
-# torch.flatten(n, m) n차원부터 시작해서 m차원까지 포함해서 flatten하라는 뜻(m의 기본값은 -1) 
-class DWConv(nn.Module):
-    def __init__(self, dim):
-        super().__init__()
-        self.dwconv = nn.Conv2d(dim, dim, 3, 1, 1, bias=True, groups=dim)
-    
-    def forward(self, x, H, W):
-        B, N, C = x.shape
-        x = x.transpose(1,2).view(B, C, H, W)
-        x = self.dwconv(x)
-        x = x.flatten(2).transpose(1,2)
-
-        return x
-
-
-
-
-
-"""
-input : image, kernal, stride, padding
-
-"""
-def OverlapPatchEmbedding():
-    return
-
 
     
 """
@@ -78,7 +53,7 @@ class EfficientSelfAttention(nn.Module):
             if m.bias is not None:
                 m.bias.data.zero_()
 
-        
+       
     
     def forward(self, x, H, W):
         B, N, C = x.shape
@@ -109,7 +84,76 @@ class EfficientSelfAttention(nn.Module):
 
 
 
+# torch.flatten(n, m) n차원부터 시작해서 m차원까지 포함해서 flatten하라는 뜻(m의 기본값은 -1) 
+class DWConv(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dwconv = nn.Conv2d(dim, dim, 3, 1, 1, bias=True, groups=dim)
     
+    def forward(self, x, H, W):
+        B, N, C = x.shape
+        x = x.transpose(1,2).view(B, C, H, W)
+        x = self.dwconv(x)
+        x = x.flatten(2).transpose(1,2)
+
+        # output : B, N', C
+        return x
+
+
+
+class MixFFN(nn.Module):
+    def __init__(self, in_features, hidden_features, out_features=None, activation=nn.GELU, drop=0.):
+        super().__init__()
+        hidden_features = in_features or hidden_features
+        out_features = in_features or out_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.dwconv = DWConv(hidden_features)
+        self.activation_layer = activation()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.dropout = nn.Dropout(drop)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.normal_(m.bias, 0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0]*m.kernel_size[1]*m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0/fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+
+    def forward(self, x, H, W):
+        x = self.fc1(x)
+        x = self.dwconv(x, H, W)
+        x = self.activation_layer(x)
+        x = self.fc2(x)
+        x = self.dropout(x)
+
+        return x
+
+
+
+class Block(nn.Module):
+    def __init__(self, dim, num_heads, qkv_bias, qk_scale, attn_drop, 
+                 proj_drop, sr_ratio, in_features, hidden_features,
+                 drop_path=0.):
+        super().__init__()
+        self.attention = EfficientSelfAttention(dim, num_heads, qkv_bias, qk_scale, attn_drop, proj_drop, sr_ratio)
+        self.mlp = MixFFN(in_features, hidden_features)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def _init_weights(self, m):
+        m
+
+
+    def forward(self, x):
+        
+        return x
 
 
 
@@ -117,6 +161,19 @@ class EfficientSelfAttention(nn.Module):
 
 
 
+
+
+
+"""
+input : image, kernal, stride, padding
+
+"""
+class OverlapPatchEmbedding(nn.Module):
+    def __init__():
+        super().__init__()
+
+    def forward():
+        return
 
 
 
