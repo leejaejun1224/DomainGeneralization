a57e035d2bb4fc9cfea225cbd79dbdaf75404fcd
leejaejun1224 <leejaejun122@naver.com>
2025-02-03 22:42:16 +0900

    transformer block finished not debugging yet

--- encoder/MiTbackbone.py
+++ encoder/MiTbackbone.py
@@ -140,26 +140,41 @@ class MixFFN(nn.Module):
 
 class Block(nn.Module):
     def __init__(self, dim, num_heads, qkv_bias, qk_scale, attn_drop, 
-                 proj_drop, sr_ratio, in_features, hidden_features,
-                 drop_path=0.):
+                 proj_drop, sr_ratio, 
+                 drop_path=0., norm_layer = nn.LayerNorm, mlp_ratio=4.,
+                 drop=0., act_layer=nn.GELU):
         super().__init__()
+
+        self.norm1 = norm_layer(dim)
         self.attention = EfficientSelfAttention(dim, num_heads, qkv_bias, qk_scale, attn_drop, proj_drop, sr_ratio)
-        self.mlp = MixFFN(in_features, hidden_features)
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        mlp_hidden_dim = int(dim*mlp_ratio)
+        self.mlp = MixFFN(in_features=dim, hidden_features=mlp_hidden_dim, 
+                          activation=act_layer, drop=drop)
+        self.norm2 = norm_layer(dim)
+        self.apply(self._init_weights)
 
     def _init_weights(self, m):
-        m
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.weight, 1.0)
+            nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
 
 
     def forward(self, x):
-        
+        x = self.drop_path(self.attention(self.norm1(x))) + x
+        x = self.drop_path(self.mlp(self.norm2(x))) + x
+
         return x
-
-
-
-
-
-
 
 
 
@@ -169,11 +184,189 @@ input : image, kernal, stride, padding
 
 """
 class OverlapPatchEmbedding(nn.Module):
-    def __init__():
+    def __init__(self, img_size, patch_size, stride, in_chans, embed_dim):
         super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        
+        self.img_size = img_size
+        self.patch_size = patch_size
 
-    def forward():
-        return
+        self.num_patches_H = img_size[0] // patch_size[0]
+        self.num_patches_W = img_size[1] // patch_size[1]
+
+        self.num_patches = self.num_patches_H * self.num_patches_W
+
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size,stride=stride,
+                              padding=(patch_size[0] // 2, patch_size[1] // 2))
+
+        self.norm = nn.LayerNorm(embed_dim)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+        
+    def forward(self, x):
+        x = self.proj(x)
+        _, _, H, W = x.shape
+
+        # B, N(H*W), C(embed_dim)
+        x = x.flatten(2).transpose(1,2)
+        x = self.norm(x)
+
+        return x, H, W
+    
+
+class MixVisionTransformer(nn.Module):
+    def __init__(self, img_size, in_chans, embed_dim,
+                 depth, num_heads, qkv_bias, qk_scale, sr_ratio,
+                 proj_drop, attn_drop, drop_path_rate, norm_layer=nn.LayerNorm):
+        super().__init__()
+        
+        self.patch_embed1 = OverlapPatchEmbedding(img_size=img_size, patch_size=7, stride=4, 
+                                                  in_chans=in_chans, embed_dim=embed_dim[0])
+        self.patch_embed2 = OverlapPatchEmbedding(img_size=img_size // 4, patch_size=3, stride=2, 
+                                                  in_chans=embed_dim[0], embed_dim=embed_dim[1])
+        self.patch_embed3 = OverlapPatchEmbedding(img_size=img_size // 8, patch_size=3, stride=2, 
+                                                  in_chans=embed_dim[1], embed_dim=embed_dim[2])
+        self.patch_embed4 = OverlapPatchEmbedding(img_size=img_size // 16, patch_size=3, stride=2, 
+                                                  in_chans=embed_dim[2], embed_dim=embed_dim[3])
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depth))]
+        cur = 0
+
+        self.block1 = nn.ModuleList([
+            Block(dim=embed_dim[0], num_heads=num_heads[0], qkv_bias=qkv_bias, 
+                  qk_scale=qk_scale, attn_drop=attn_drop[0], proj_drop=proj_drop[0], sr_ratio=sr_ratio[0], 
+                  drop_path=dpr[cur + i], 
+                  norm_layer = nn.LayerNorm, mlp_ratio=4., drop=0., 
+                  act_layer=nn.GELU)
+        for i in range(depth[0])])
+        self.norm1 = norm_layer(embed_dim[0])
+
+        cur += depth[0]
+        self.block2 = nn.ModuleList([
+            Block(dim=embed_dim[1], num_heads=num_heads[1], qkv_bias=qkv_bias, 
+                  qk_scale=qk_scale, attn_drop=attn_drop[1], proj_drop=proj_drop[1], sr_ratio=sr_ratio[1], 
+                  drop_path=dpr[cur + i], 
+                  norm_layer = nn.LayerNorm, mlp_ratio=4., drop=0., 
+                  act_layer=nn.GELU)
+        for i in range(depth[1])])
+        self.norm2 = norm_layer(embed_dim[1])
+
+
+        cur += depth[1]
+        self.block3 = nn.ModuleList([
+            Block(dim=embed_dim[2], num_heads=num_heads[2], qkv_bias=qkv_bias, 
+                  qk_scale=qk_scale, attn_drop=attn_drop[2], proj_drop=proj_drop[2], sr_ratio=sr_ratio[2], 
+                  drop_path=dpr[cur + i], 
+                  norm_layer = nn.LayerNorm, mlp_ratio=4., drop=0., 
+                  act_layer=nn.GELU)
+        for i in range(depth[2])])
+        self.norm3 = norm_layer(embed_dim[2])
 
 
 
+        cur += depth[2]
+        self.block4 = nn.ModuleList([
+            Block(dim=embed_dim[3], num_heads=num_heads[3], qkv_bias=qkv_bias, 
+                  qk_scale=qk_scale, attn_drop=attn_drop[3], proj_drop=proj_drop[3], sr_ratio=sr_ratio[3], 
+                  drop_path=dpr[cur + i], 
+                  norm_layer = nn.LayerNorm, mlp_ratio=4., drop=0., 
+                  act_layer=nn.GELU)
+        for i in range(depth[3])])
+        self.norm4 = norm_layer(embed_dim[3])
+
+        self.apply(self._init_weights)
+
+
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+        
+
+
+    def forward_feature(self, x):
+        B = x.shape[0]
+        output = []
+
+        # stage 1
+        x, H, W = self.patch_embed1(x)
+        for i, blk in enumerate(self.block1):
+            x = blk(x)
+        # x : [B, N, C]
+        x = self.norm1(x)
+        x = x.reshape(B, H, W, -1).transpose(0, 3, 1, 2).contiguous()
+        output.append(x)
+
+
+        # stage 2
+        x, H, W = self.patch_embed2(x)
+        for i, blk in enumerate(self.block2):
+            x = blk(x)
+        # x : [B, N, C]
+        x = self.norm2(x)
+        x = x.reshape(B, H, W, -1).transpose(0, 3, 1, 2).contiguous()
+        output.append(x)
+
+
+        # stage 3
+        x, H, W = self.patch_embed3(x)
+        for i, blk in enumerate(self.block3):
+            x = blk(x)
+        # x : [B, N, C]
+        x = self.norm3(x)
+        x = x.reshape(B, H, W, -1).transpose(0, 3, 1, 2).contiguous()
+        output.append(x)
+
+
+        # stage 4
+        x, H, W = self.patch_embed4(x)
+        for i, blk in enumerate(self.block4):
+            x = blk(x)
+        # x : [B, N, C]
+        x = self.norm4(x)
+        x = x.reshape(B, H, W, -1).transpose(0, 3, 1, 2).contiguous()
+        output.append(x)
+
+        return output
+    
+
+    def forward_head(self, x):
+        return x
+
+
+    def forward(self, x):
+        x = self.forward_feature(x)
+        x = self.forward_head(x)
+        return x
+
+
+
+# if __name__=="__main__":
+#     dpr = [x.item() for x in torch.linspace(0, 0.1, 16)]
+#     print(len(dpr))
