99458215a88b009b78f480e61c8609f4e5c1a270
leejaejun1224 <leejaejun122@naver.com>
2025-02-12 20:37:50 +0900

    cityscapes to kitti

---
+++ config/datasets/cityscapes_to_kitti2015.py
@@ -0,0 +1,65 @@
+
+
+kitti_2015_pipeline = [
+    dict(type='Resize', img_scale=(1248, 384))
+]
+
+kitti_2012_pipeline = [
+    dict(type='Resize', img_scale=(1248, 384))
+]
+
+cityscapes_pipeline = [
+    dict(type='Resize', img_scale=(1248, 384))
+]
+
+source_dataset = 'cityscapes'
+# target_dataset = 'kitti_2015'
+target_dataset = 'kitti_2015'
+
+dataset = dict(
+    train = dict(
+        type='UDAdataset',
+        source=dict(
+            data_root=f'/home/jaejun/dataset/cityscapes',
+            file_list=f'./filenames/source/{source_dataset}_train.txt',
+            pipeline=cityscapes_pipeline
+        ),
+        target=dict(
+            data_root=f'/home/jaejun/dataset/kitti',
+            file_list=f'./filenames/target/{target_dataset}_train.txt',
+            pipeline=kitti_2015_pipeline
+        )
+    ),
+    val = dict(
+        type='UDAdataset',
+        source=dict(
+            data_root=f'/home/jaejun/dataset/cityscapes',
+            file_list=f'./filenames/source/{source_dataset}_val.txt',
+            pipeline=kitti_2015_pipeline
+        ),
+        target=dict(
+            data_root=f'/home/jaejun/dataset/kitti',
+            file_list=f'./filenames/target/{target_dataset}_val.txt',
+            pipeline=cityscapes_pipeline
+        )
+    ),
+    test = dict(
+        type='UDAdataset',
+        source=dict(
+            data_root=f'/home/jaejun/dataset/cityscapes',
+            left_img_dir='image_2',
+            right_img_dir='image_3',
+            ann_dir='2015 disparity gt 저장된 곳',
+            file_list=f'./filenames/source/{source_dataset}_test.txt',
+            pipeline=kitti_2015_pipeline
+        ),
+        target=dict(
+            data_root=f'/home/jaejun/dataset/kitti',
+            left_img_dir='image_2',
+            right_img_dir='image_3',
+            ann_dir='2012 disparity gt 저장된 곳',
+            file_list=f'./filenames/target/{target_dataset}_test.txt',
+            pipeline=cityscapes_pipeline
+        )
+    )
+)
---
+++ config/uda/kit15_cityscapes.py
@@ -0,0 +1,35 @@
+
+
+
+depth_model = dict(
+    name = 'Fast_ACVNet',
+    maxdisp = 192,
+    att_weights_only = False
+)
+
+uda = dict(
+    threshold = 0.3,
+    alpha = 0.99,
+    train_source_only = True
+)
+
+optimizer = dict(
+    optimizer = "Adam",
+    lr = 1e-4
+)
+
+# name_dataset = 'kitti2015_to_kitti2012'
+name_dataset = 'kitti2015_to_cityscapes'
+
+
+data = dict(
+    train = dict(
+        epoch = 200,
+        batch_size = 1,
+        num_workers = 2,
+        shuffle = True,
+        pin_memory = True,
+        val_interval = 5 
+    )
+)
+
