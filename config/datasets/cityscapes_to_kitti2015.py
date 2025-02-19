3b0d2c199e5fa57870b43ab8a86bce5850e5c91a
leejaejun1224 <leejaejun122@naver.com>
2025-02-19 21:49:42 +0900

    filename

--- config/datasets/cityscapes_to_kitti2015.py
+++ config/datasets/cityscapes_to_kitti2015.py
@@ -17,51 +17,51 @@ source_dataset = 'cityscapes'
 target_dataset = 'kitti_2015'
 
 
-cityscapes_train = {}
-cityscapes_train['data_root'] = '/home/jaejun/dataset/cityscapes'
-cityscapes_train['file_list'] = './filenames/source/{source_dataset}_train.txt'
-cityscapes_train['pipeline'] = cityscapes_pipeline
+source_train = {}
+source_train['data_root'] = f'/home/jaejun/dataset/{source_dataset}'
+source_train['file_list'] = f'./filenames/source/{source_dataset}_train.txt'
+source_train['pipeline'] = cityscapes_pipeline
 
-cityscapes_val = {}
-cityscapes_val['data_root'] = '/home/jaejun/dataset/cityscapes'
-cityscapes_val['file_list'] = './filenames/source/{source_dataset}_val.txt'
-cityscapes_val['pipeline'] = cityscapes_pipeline
+source_val = {}
+source_val['data_root'] = f'/home/jaejun/dataset/{source_dataset}'
+source_val['file_list'] = f'./filenames/source/{source_dataset}_val.txt'
+source_val['pipeline'] = cityscapes_pipeline
 
-cityscapes_test = {}
-cityscapes_test['data_root'] = '/home/jaejun/dataset/cityscapes'
-cityscapes_test['file_list'] = './filenames/source/{source_dataset}_test.txt'
-cityscapes_test['pipeline'] = cityscapes_pipeline
+source_test = {}
+source_test['data_root'] = f'/home/jaejun/dataset/{source_dataset}'
+source_test['file_list'] = f'./filenames/source/{source_dataset}_test.txt'
+source_test['pipeline'] = cityscapes_pipeline
 
-kitti_train = {}
-kitti_train['data_root'] = '/home/jaejun/dataset/kitti'
-kitti_train['file_list'] = './filenames/target/{target_dataset}_train.txt'
-kitti_train['pipeline'] = kitti_2015_pipeline
+target_train = {}
+target_train['data_root'] = f'/home/jaejun/dataset/{target_dataset}'
+target_train['file_list'] = f'./filenames/target/{target_dataset}_train.txt'
+target_train['pipeline'] = kitti_2015_pipeline
 
-kitti_val = {}
-kitti_val['data_root'] = '/home/jaejun/dataset/kitti'
-kitti_val['file_list'] = './filenames/target/{target_dataset}_val.txt'
-kitti_val['pipeline'] = kitti_2015_pipeline
+target_val = {}
+target_val['data_root'] = f'/home/jaejun/dataset/{target_dataset}'
+target_val['file_list'] = f'./filenames/target/{target_dataset}_val.txt'
+target_val['pipeline'] = kitti_2015_pipeline
 
-kitti_test = {}
-kitti_test['data_root'] = '/home/jaejun/dataset/kitti'
-kitti_test['file_list'] = './filenames/target/{target_dataset}_test.txt'
-kitti_test['pipeline'] = kitti_2015_pipeline
+target_test = {}
+target_test['data_root'] = f'/home/jaejun/dataset/{target_dataset}'
+target_test['file_list'] = f'./filenames/target/{target_dataset}_test.txt'
+target_test['pipeline'] = kitti_2015_pipeline
 
 dataset = dict(
     train = dict(
         type='UDAdataset',
-        source=cityscapes_train,
-        target=kitti_train
+        source=source_train,
+        target=target_train
     ),
     val = dict(
         type='UDAdataset',
-        source=cityscapes_val,
-        target=kitti_val
+        source=source_val,
+        target=target_val
     ),
     test = dict(
         type='UDAdataset',
-        source=cityscapes_test,
-        target=kitti_test
+        source=source_test,
+        target=target_test
         )
     )
 
