29e2ddf8b9a20b2d0a0865f76334e7734d528e2b
leejaejun1224 <leejaejun122@naver.com>
2025-03-15 17:34:35 +0900

    logger

--- test.py
+++ test.py
@@ -9,30 +9,24 @@ import torch.nn as nn
 import torch.backends.cudnn as cudnn
 import matplotlib.pyplot as plt
 from models.uda import __models__
-
-
 from torch.utils.data import DataLoader
 from datasets import __datasets__
 from datasets.dataloader import PrepareDataset
 from experiment import prepare_cfg
 from tools.metrics import EPE_metric, D1_metric, Thres_metric, tensor2float
-from tools.write_log import save_disparity, save_metrics, save_att, compare, save_entropy, save_gt
-from tools.save_heatmap import save_heatmap
-from models.estimator.Fast_ACV_plus import Feature
+from tools.write_log import Logger
 from collections import OrderedDict
 
 cudnn.benchmark = True
 os.environ['CUDA_VISIBLE_DEVICES'] = '0, 1'
 
-
-
-def main():
+def setup_args():
     parser = argparse.ArgumentParser(description="StereoDepth Unsupervised Domain Adaptation")
     parser.add_argument('--dataset_config', default='./config/datasets/cityscapes_to_kitti2015.py', help='source domain and target domain name')
     parser.add_argument('--uda_config', default='./config/uda/kit15_cityscapes.py', help='UDA model preparation')
     parser.add_argument('--seed', default=1, metavar='S', help='random seed(default = 1)')
     parser.add_argument('--log_dir', default='./log', help='log directory')
-    parser.add_argument('--ckpt', default='', help='checkpoint', )
+    parser.add_argument('--ckpt', default='', help='checkpoint')
     parser.add_argument('--compute_metrics', default=True, help='compute error')
     parser.add_argument('--save_disp', default=True, help='save disparity')
     parser.add_argument('--save_att', default=True, help='save attention')
@@ -40,105 +34,82 @@ def main():
     parser.add_argument('--save_entropy', default=True, help='save entropy')
     parser.add_argument('--save_gt', default=True, help='save gt')
     parser.add_argument('--compare_costvolume', default=True, help='compare costvolume')
-    args = parser.parse_args()
+    return parser.parse_args()
+
+def setup_model(cfg, ckpt_path):
+    model = __models__['StereoDepthUDA'](cfg)
+    checkpoint = torch.load(ckpt_path)
+    model.student_model.load_state_dict(checkpoint['student_state_dict'])
+    model.teacher_model.load_state_dict(checkpoint['teacher_state_dict'])
+    model.to('cuda:0')
+    return model
+
+def process_batch(data_batch):
+    for key in data_batch:
+        if isinstance(data_batch[key], torch.Tensor):
+            data_batch[key] = data_batch[key].cuda()
+    return data_batch
+
+def compute_metrics_dict(data_batch, mask):
+    scalar_outputs = {
+        "EPE": [EPE_metric(data_batch['src_pred_disp'][0], data_batch['src_disparity'], mask)],
+        "D1": [D1_metric(data_batch['src_pred_disp'][0], data_batch['src_disparity'], mask)],
+        "Thres1": [Thres_metric(data_batch['src_pred_disp'][0], data_batch['src_disparity'], mask, 1.0)],
+        "Thres2": [Thres_metric(data_batch['src_pred_disp'][0], data_batch['src_disparity'], mask, 2.0)],
+        "Thres3": [Thres_metric(data_batch['src_pred_disp'][0], data_batch['src_disparity'], mask, 3.0)]
+    }
+    return scalar_outputs
+
+def main():
+    args = setup_args()
     assert args.ckpt != '', 'checkpoint is required !!'
 
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
-    dir_name = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M")
-    log_dir  = '/'.join(args.ckpt.split('/')[:-1])
-    save_dir = log_dir + '/disp'
     
+    log_dir = '/'.join(args.ckpt.split('/')[:-1])
+    save_dir = os.path.join(log_dir, 'disp')
     os.makedirs(save_dir, exist_ok=True)
 
     cfg = prepare_cfg(args, mode='test')
-    log_dict = {'parameters': cfg}
-
-    test_dataset = PrepareDataset(source_datapath=cfg['dataset']['src_root'],
-                                target_datapath=cfg['dataset']['tgt_root'],
-                                sourcefile_list=cfg['dataset']['src_filelist'], 
-                                targetfile_list=cfg['dataset']['tgt_filelist'],
-                                training=False)
-    test_loader = DataLoader(test_dataset, batch_size=cfg['test_batch_size'], shuffle=False, num_workers=cfg['test_num_workers'], drop_last=False)
-    # print(cfg)
-
-    model = __models__['StereoDepthUDA'](cfg)
     
-    model.student_model.load_state_dict(torch.load(args.ckpt)['student_state_dict'])
-    model.teacher_model.load_state_dict(torch.load(args.ckpt)['teacher_state_dict'])
+    test_dataset = PrepareDataset(
+        source_datapath=cfg['dataset']['src_root'],
+        target_datapath=cfg['dataset']['tgt_root'],
+        sourcefile_list=cfg['dataset']['src_filelist'],
+        targetfile_list=cfg['dataset']['tgt_filelist'],
+        training=False
+    )
     
-    # checkpoint = torch.load(args.ckpt, map_location="cuda:0")
-    # new_state_dict = OrderedDict()
-    # for k, v in checkpoint['model'].items():
-    #     name = k.replace("module.", "")  # DataParallel로 저장된 모델 처리
-    #     new_state_dict[name] = v
-    # model.student_model.load_state_dict(new_state_dict, strict=True)
-    # model.teacher_model.load_state_dict(new_state_dict, strict=True)
+    test_loader = DataLoader(
+        test_dataset, 
+        batch_size=cfg['test_batch_size'],
+        shuffle=False,
+        num_workers=cfg['test_num_workers'],
+        drop_last=False
+    )
 
-
-
-    model.to('cuda:0')
-    num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print(f"Number of trainable parameters: {num_params}")
-    
-    # 이거 init하는 조건은 좀 더 생각을 해봐야겠는데
-
-    # optimizer 좀 더 고민해보자.
-    # 시작하자잉
-    # target에 대해서 metric를 구할 줄 알아야 한다.
-    train_losses = []
-    step_loss = {}
-    metrics_dict = {}
+    model = setup_model(cfg, args.ckpt)
     model.eval()
-    for batch_idx, data_batch in enumerate(test_loader):
-        # print(data_batch)
-        source_filename = data_batch['source_left_filename'][0].split('/')[-1]
+    
+    metrics_dict = {}
+    logger = Logger(save_dir)
+    logger._setup_directories()
+    
+    with torch.no_grad():
+        for _, data_batch in enumerate(test_loader):
+            src = True if 'src_disparity' in data_batch.keys() else False
+            tgt = True if 'tgt_disparity' in data_batch.keys() else False
 
-        for key in data_batch:
-            if isinstance(data_batch[key], torch.Tensor):
-                data_batch[key] = data_batch[key].cuda()
-                # print(data_batch[key])
-        log_vars = model.forward_test(data_batch)
+            source_filename = data_batch['source_left_filename'][0].split('/')[-1]
+            data_batch = process_batch(data_batch)
+            log_vars = model.forward_test(data_batch)
 
-        if args.save_entropy:
-            save_entropy(data_batch, log_dir)
+            for idx in range(cfg['test_batch_size']):
+                logger.log(data_batch[idx], log_vars)
 
-        if args.compare_costvolume:
-            compare(data_batch, log_dir)
-            # [batch, 12, 1, 48, 156]
-            # print("entropy_map shape: ", data_batch['src_shape_map'].shape)
-        if args.save_gt and 'src_disparity' in data_batch.keys():
-            save_gt(data_batch, log_dir)
-
-        if args.save_att:
-            save_att(data_batch, log_dir)
-
-        if args.save_disp:
-            save_disparity(data_batch, log_dir)
-
-        if args.save_heatmap:   
-            image_tensor = data_batch['src_left']
-            feature_map, attn_weights = model.student_model.feature(image_tensor)
-            save_heatmap(image_tensor, feature_map, attn_weights)
-
-        if args.compute_metrics and 'src_disparity' in data_batch.keys():
-            scalar_outputs = {}
-            scalar_outputs["EPE"] = [EPE_metric(data_batch['src_pred_disp'][0], data_batch['src_disparity'], data_batch['mask'])]
-            scalar_outputs["D1"] = [D1_metric(data_batch['src_pred_disp'][0], data_batch['src_disparity'], data_batch['mask'])]
-            scalar_outputs["Thres1"] = [Thres_metric(data_batch['src_pred_disp'][0], data_batch['src_disparity'], data_batch['mask'], 1.0)]
-            scalar_outputs["Thres2"] = [Thres_metric(data_batch['src_pred_disp'][0], data_batch['src_disparity'], data_batch['mask'], 2.0)]
-            scalar_outputs["Thres3"] = [Thres_metric(data_batch['src_pred_disp'][0], data_batch['src_disparity'], data_batch['mask'], 3.0)]
-            metrics_dict[source_filename] = tensor2float(scalar_outputs)
-
-    save_metrics(metrics_dict, log_dir)
-            
-
+    logger.save_metrics(metrics_dict, log_dir)
     return 0
 
-
-
-
-
 if __name__=="__main__":
-    # argparser
     main()
--- tools/write_log.py
+++ tools/write_log.py
@@ -7,137 +7,104 @@ import matplotlib.pyplot as plt
 import torch.nn.functional as F
 from skimage import exposure
 
-def save_att(data_batch, dir_name):
-    os.makedirs(dir_name + '/att', exist_ok=True)
-    # print("data_batch['src_pred_disp']", len(data_batch['src_pred_disp']))
+class Logger:
+    
+    def __init__(self, save_dir):
+        self.save_dir = save_dir
+        self._setup_directories()
 
-    # this for loop is for batch size
-    for i in range(data_batch['src_pred_disp'][0].shape[0]):
-        att_prob, _ = data_batch['src_pred_disp'][2][i].max(dim=0, keepdim=True)
+
+    def _setup_directories(self):
+        self.att_dir = os.path.join(self.save_dir, 'att')
+        self.gt_dir = os.path.join(self.save_dir, 'save_gt') 
+        self.disp_dir_src = os.path.join(self.save_dir, 'disp', 'src')
+        self.disp_dir_tgt = os.path.join(self.save_dir, 'disp', 'tgt')
+        self.entropy_dir = os.path.join(self.save_dir, 'entropy')
+        self.metrics_dir = os.path.join(self.save_dir, 'metrics')
+
+        os.makedirs(self.att_dir, exist_ok=True)
+        os.makedirs(self.gt_dir, exist_ok=True)
+        os.makedirs(self.disp_dir_src, exist_ok=True)
+        os.makedirs(self.disp_dir_tgt, exist_ok=True)
+        os.makedirs(self.entropy_dir, exist_ok=True)
+        os.makedirs(self.metrics_dir, exist_ok=True)
+
+
+
+    def _save_image(self, data, filename, directory, cmap='gray'):
+        plt.imsave(os.path.join(directory, filename), data, cmap=cmap)
+
+
+    def save_att(self, data_batch):
+        att_prob, _ = data_batch['src_pred_disp'][2].max(dim=0, keepdim=True)
         att_prob = att_prob.squeeze(0).cpu().numpy()
-        source_left_filename = data_batch['source_left_filename'][i].split('/')[-1]
-        plt.imsave(os.path.join(dir_name, 'att', source_left_filename), att_prob, cmap='gray')
-
-def save_gt(data_batch, dir_name):
-    os.makedirs(dir_name + '/save_gt', exist_ok=True)
-    for i in range(data_batch['src_pred_disp'][0].shape[0]):
-        source_left_filename = data_batch['source_left_filename'][i].split('/')[-1]
-        plt.imsave(os.path.join(dir_name, 'save_gt', source_left_filename), data_batch['src_disparity'][i].cpu().numpy(), cmap='jet')
+        filename = data_batch['source_left_filename'].split('/')[-1]
+        self._save_image(att_prob, filename, self.att_dir)
 
 
-def save_disparity(data_batch, dir_name):
-    os.makedirs(dir_name + '/disp/src', exist_ok=True)
-    os.makedirs(dir_name + '/disp/tgt', exist_ok=True)
-    pred_src_dir = os.path.join(dir_name, 'disp', 'src')
-    os.makedirs(pred_src_dir, exist_ok=True)
-    for i in range(data_batch['src_pred_disp'][0].shape[0]):
-
-        pred_src = data_batch['src_pred_disp'][0][i].cpu().numpy()
-        source_left_filename = data_batch['source_left_filename'][i].split('/')[-1]
-        plt.imsave(os.path.join(pred_src_dir, source_left_filename), pred_src, cmap='jet')
-
-        pred_tgt_dir = os.path.join(dir_name, 'disp', 'tgt')
-        os.makedirs(pred_tgt_dir, exist_ok=True)
-        pred_tgt = data_batch['tgt_pred_disp'][0][i].cpu().numpy()
-        target_left_filename = data_batch['target_left_filename'][i].split('/')[-1] 
-        plt.imsave(os.path.join(pred_tgt_dir, target_left_filename), pred_tgt, cmap='jet')
+    def save_gt(self, data_batch):
+        ### just for debug and eye check
+        filename = data_batch['source_left_filename'].split('/')[-1]
+        gt_disp = data_batch['src_disparity'].cpu().numpy()
+        self._save_image(gt_disp, filename, self.gt_dir, cmap='jet')
 
 
-def compare(data_batch, dir_name):
-    cost_volume_compare_dir = os.path.join(dir_name, 'cost_volume_compare')
-    os.makedirs(cost_volume_compare_dir, exist_ok=True)
-    if 'src_disparity' in data_batch.keys():
-        for i in range(data_batch['src_pred_disp'][0].shape[0]):
+    def save_disparity(self, data_batch):
+        pred_src = data_batch['src_pred_disp'][0].cpu().numpy()
+        src_filename = data_batch['source_left_filename'].split('/')[-1]
+        self._save_image(pred_src, src_filename, self.disp_dir_src, cmap='jet')
 
-            source_left_filename = data_batch['source_left_filename'][i].split('/')[-1]
-            shape_map_resized = F.interpolate(data_batch['src_shape_map'][i].float(), scale_factor=4, mode="nearest")
-            shape_map_resized = shape_map_resized.squeeze(0).squeeze(0).cpu().numpy()
-            disparity = data_batch['src_disparity'][i].cpu().numpy()
+        pred_tgt = data_batch['tgt_pred_disp'][0].cpu().numpy()
+        tgt_filename = data_batch['target_left_filename'].split('/')[-1]
+        self._save_image(pred_tgt, tgt_filename, self.disp_dir_tgt, cmap='jet')
 
 
-            if shape_map_resized.shape != disparity.shape:
-                print("shape_map_resized shape : ", shape_map_resized.shape)
-                print("disparity shape : ", disparity.shape)
-
-            diff = np.abs(shape_map_resized - disparity)
-            mask = np.where(diff <= 1, disparity, 0).astype(disparity.dtype)
-            plt.imsave(os.path.join(cost_volume_compare_dir, source_left_filename), mask, cmap='jet')
-
-def save_entropy(data_batch, dir_name):
-    # 저장할 디렉터리 생성
-    entropy_dir = os.path.join(dir_name, 'entropy')
-    os.makedirs(entropy_dir, exist_ok=True)
-
-    for i in range(data_batch['src_pred_disp'][0].shape[0]):
-        shape_map = data_batch['tgt_shape_map'][i]
-
-        # 그룹 평균 계산
-        # shape_map_avg = shape_map.mean(dim=0, keepdim=True)
-
-        # 해상도 확대 (scale_factor=4) 및 차원 축소
+    def save_entropy(self, data_batch):
+        shape_map = data_batch['tgt_shape_map']
         shape_map_resized = F.interpolate(shape_map.float(), scale_factor=4, mode="nearest")
         shape_map_resized = shape_map_resized.squeeze(0).squeeze(0).cpu().numpy()
         
-        # map_min = shape_map_resized.min()
-        # map_max = shape_map_resized.max()
-        # shape_map_norm = (shape_map_resized - map_min) / (map_max - map_min + 1e-8)
+        filename = data_batch['target_left_filename'].split('/')[-1]
+        save_path = os.path.join(self.entropy_dir, filename)
 
-        # # 차이를 더 강조하기 위해 추가 스케일링
-        # alpha = 2.0  # 예: 2배 확대
-        # shape_map_scaled = shape_map_norm * alpha
-        # 스케일링 이후 1을 초과한 값은 1로 클리핑
-        # shape_map_scaled = torch.clamp(shape_map_scaled, 0, 1)
-        # 저장할 파일 이름 설정
-        target_left_filename = data_batch['target_left_filename'][i].split('/')[-1]
-        save_path = os.path.join(entropy_dir, target_left_filename)
-
-        # 컬러바 포함 이미지 저장 (컬러바 크기 조절)
+        # Create figure with colorbar
         plt.figure(figsize=(12, 8))
         img = plt.imshow(shape_map_resized, cmap='jet')
-        
-
-
-        # 컬러바 추가 (크기 조절)
         cbar = plt.colorbar(img, fraction=0.015, pad=0.04)
-        cbar.ax.tick_params(labelsize=8)  # 컬러바 숫자 크기 조절
-
-        plt.axis('off')  # 축을 없애서 깔끔하게 저장
+        cbar.ax.tick_params(labelsize=8)
+        plt.axis('off')
         plt.savefig(save_path, bbox_inches='tight', pad_inches=0.1)
         plt.close()
 
+    def save_metrics(self, metrics):
+        totals = {metric: 0 for metric in ['EPE', 'D1', 'Thres1', 'Thres2', 'Thres3']}
+        count = 0
 
+        # 모든 이미지에 대해서 다 
+        for disp_metrics in metrics.values():
+            if all(key in disp_metrics for key in totals.keys()):
+                for metric in totals:
+                    totals[metric] += disp_metrics[metric][0]
+                count += 1
 
-        # plt.imsave(os.path.join(entropy_dir, source_left_filename), shape_map_resized.cpu().numpy(), cmap='jet')
-
-def save_metrics(metrics, dir_name):
-    total_epe = 0
-    total_d1 = 0
-    total_thres1 = 0
-    total_thres2 = 0
-    total_thres3 = 0
-    count = 0
-
-    for disp_metrics in metrics.values():
-        if all(key in disp_metrics for key in ['EPE', 'D1', 'Thres1', 'Thres2', 'Thres3']):
-            total_epe += disp_metrics['EPE'][0]
-            total_d1 += disp_metrics['D1'][0]
-            total_thres1 += disp_metrics['Thres1'][0]
-            total_thres2 += disp_metrics['Thres2'][0]
-            total_thres3 += disp_metrics['Thres3'][0]
-            count += 1
-
-    averages = {
-        'average_metric' : {
-            'EPE' : total_epe / count if count > 0 else 0,
-            'D1' : total_d1 / count if count > 0 else 0,
-            'Thres1' : total_thres1 / count if count > 0 else 0,
-            'Thres2' : total_thres2 / count if count > 0 else 0,
-            'Thres3' : total_thres3 / count if count > 0 else 0
+        # 평균
+        averages = {
+            'average_metric': {
+                metric: totals[metric] / count if count > 0 else 0 
+                for metric in totals
+            }
         }
-    }
 
-    metrics.update(averages)
-    metrics_dir = os.path.join(dir_name, 'metrics')
-    os.makedirs(metrics_dir, exist_ok=True)
-    with open(os.path.join(metrics_dir, 'metrics.json'), 'w') as f:
-        json.dump(metrics, f, indent=4)
+        # metrics
+        metrics.update(averages)
+        os.makedirs(self.metrics_dir, exist_ok=True)
+        with open(os.path.join(self.metrics_dir, 'metrics.json'), 'w') as f:
+            json.dump(metrics, f, indent=4)
+
+
+    def log(self, data_batch, metrics):
+        self.save_entropy(data_batch)
+        self.save_gt(data_batch)
+        self.save_att(data_batch)
+        self.save_disparity(data_batch)
+        self.save_metrics(metrics)
