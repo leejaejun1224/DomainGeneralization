e1d68b016ab7dfede4e9d7fdace707e12981f388
leejaejun1224 <leejaejun122@naver.com>
2025-08-29 23:24:40 +0900

    folder name

--- config/datasets/cityscapes_to_kitti2015.py
+++ config/datasets/cityscapes_to_kitti2015.py
@@ -12,8 +12,8 @@ cityscapes_pipeline = [
     dict(type='Resize', img_scale=(1248, 384))
 ]
 
-source_dataset = 'kitti_2015'
-# source_dataset = 'flyingthing'
+# source_dataset = 'kitti_2015'
+source_dataset = 'flyingthing'
 # source_dataset = 'cityscapes'
 # source_dataset = 'driving_stereo'
 
@@ -24,44 +24,44 @@ target_dataset = 'kitti_2015'
 
 source_train = {}
 source_train['type'] = f'{source_dataset}'
-source_train['data_root'] = f'~/dataset/{source_dataset}'
+source_train['data_root'] = f'~/leejj/dataset/{source_dataset}'
 source_train['file_list'] = f'./filenames/source/{source_dataset}_train.txt'
-source_train['prior'] = f'~/dataset/{source_dataset}/disparity_distribution_{source_dataset}.npy'
+source_train['prior'] = f'~/leejj/dataset/{source_dataset}/disparity_distribution_{source_dataset}.npy'
 source_train['pipeline'] = cityscapes_pipeline
 
 source_val = {}
 source_val['type'] = f'{source_dataset}'
-source_val['data_root'] = f'~/dataset/{source_dataset}'
+source_val['data_root'] = f'~/leejj/dataset/{source_dataset}'
 source_val['file_list'] = f'./filenames/source/{source_dataset}_val.txt'
-source_val['prior'] = f'~/dataset/{source_dataset}/disparity_distribution_{source_dataset}.npy'
+source_val['prior'] = f'~/leejj/dataset/{source_dataset}/disparity_distribution_{source_dataset}.npy'
 source_val['pipeline'] = cityscapes_pipeline
 
 source_test = {}
 source_test['type'] = f'{source_dataset}'
-source_test['data_root'] = f'~/dataset/{source_dataset}'
+source_test['data_root'] = f'~/leejj/dataset/{source_dataset}'
 source_test['file_list'] = f'./filenames/source/{source_dataset}_test.txt'
-source_test['prior'] = f'~/dataset/{source_dataset}/disparity_distribution_{source_dataset}.npy'
+source_test['prior'] = f'~/leejj/dataset/{source_dataset}/disparity_distribution_{source_dataset}.npy'
 source_test['pipeline'] = cityscapes_pipeline
 
 target_train = {}
 target_train['type'] = f'{target_dataset}'
-target_train['data_root'] = f'~/dataset/{target_dataset}'
+target_train['data_root'] = f'~/leejj/dataset/{target_dataset}'
 target_train['file_list'] = f'./filenames/target/{target_dataset}_train.txt'
-target_train['prior'] = f'~/dataset/{target_dataset}/disparity_distribution_{target_dataset}.npy'
+target_train['prior'] = f'~/leejj/dataset/{target_dataset}/disparity_distribution_{target_dataset}.npy'
 target_train['pipeline'] = kitti_2015_pipeline
 
 target_val = {}
 target_val['type'] = f'{target_dataset}'
-target_val['data_root'] = f'~/dataset/{target_dataset}'
+target_val['data_root'] = f'~/leejj/dataset/{target_dataset}'
 target_val['file_list'] = f'./filenames/target/{target_dataset}_val.txt'
-target_val['prior'] = f'~/dataset/{target_dataset}/disparity_distribution_{target_dataset}.npy'
+target_val['prior'] = f'~/leejj/dataset/{target_dataset}/disparity_distribution_{target_dataset}.npy'
 target_val['pipeline'] = kitti_2015_pipeline
 
 target_test = {}
 target_test['type'] = f'{target_dataset}'
-target_test['data_root'] = f'~/dataset/{target_dataset}'
+target_test['data_root'] = f'~/leejj/dataset/{target_dataset}'
 target_test['file_list'] = f'./filenames/target/{target_dataset}_test.txt'
-target_test['prior'] = f'~/dataset/{target_dataset}/disparity_distribution_{target_dataset}.npy'
+target_test['prior'] = f'~/leejj/dataset/{target_dataset}/disparity_distribution_{target_dataset}.npy'
 target_test['pipeline'] = kitti_2015_pipeline
 
 dataset = dict(
